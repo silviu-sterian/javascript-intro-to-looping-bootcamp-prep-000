a77b5d99d74000d64d9b6bab549d9ef433de9ff2
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-23 01:06:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,20 +25,15 @@ function forLoop(arr)
 
 function whileLoop(n)
 {
-  while()
+  while(n>0)
+  {
+    console.log(n);
+    n--;
+  }
+  return 'done';
 }
 
-  describe('whileLoop(n)', () => {
-    it('counts down from n to 0', () => {
-      const spy = chai.spy.on(console, 'log')
-      const n = Math.floor(Math.random() * 100)
-
-      expect(whileLoop(n)).to.equal('done')
-      expect(spy).to.have.been.called.exactly(n)
-
-      console.log.reset()
-    })
-  })
+ 
 
   describe('doWhileLoop(array)', () => {
     it('removes describe('forLoop(array)', () => {
